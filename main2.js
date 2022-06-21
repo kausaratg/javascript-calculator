df264b26927197a009c0915e1e4778e39b8d89d5
kausaratg <kausaratg@gmail.com>
2022-06-21 22:34:51 +0200

    first commit

---
+++ main2.js
@@ -0,0 +1,19 @@
+num1 = 3
+num2 = 4
+sign = '+'
+if (sign == '+'){
+    result = num1 + num2
+}
+else if (sign == '-'){
+    result = num1 - num2
+}
+else if (sign == '*'){
+    result = num1 * num2
+}
+else if (sign == '/'){
+    result == num1 / num2
+}
+else{
+    result = 'wrong sign'
+}
+console.log(result)
